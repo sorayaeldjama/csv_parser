4f08bdc78de2bb02bed2532caed7145175cb2b00
soraya <sorayaeldjama@gmail.com>
2021-04-05 20:48:12 +0100

    commit 7

--- csv_parser/file_handler.py
+++ csv_parser/file_handler.py
@@ -11,17 +11,18 @@ for url in ['https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv', 'http
         chaine = url.split("/")
         name_file = chaine[len(chaine) - 1] 
         name_file = r"C:\Users\soray\Documents\cours python\Projets\csv-parser\csv_parser\csv_uploads"
-        while not os.path.isfile(name_file) {
-            if not os.path.isfile(name_file){
-                with open(name_file, 'wb') as file:
-                    # get request
-                    response = requests.get(url) # response est un objet qui permet d'inspecter le resultat de la requete 
-                    # write to file
-                    file.write(response.content)
-                    # si response est successful, no Exception will be raised 
-                    response.raise_for_status() # un HTTPErrorsera déclenché pour certains codes d'état si le code d'etat indique reussit il continu 
+       
+        if not os.path.isfile(name_file){
+            with open(name_file, 'wb') as file:
+                # get request
+                response = requests.get(url) # response est un objet qui permet d'inspecter le resultat de la requete 
+                # write to file
+                file.write(response.content)
+                # si response est successful, no Exception will be raised 
+                response.raise_for_status() # un HTTPErrorsera déclenché pour certains codes d'état si le code d'etat indique reussit il continu 
             }
-        }
+           
+       
     except HTTPError as http_err:
                 print(f'HTTP error occurred: {http_err}')   
     except Exception as err:
