4b8efff5000e50cbc149a12c43f46a3a300c72eb
soraya <sorayaeldjama@gmail.com>
2021-04-06 20:19:04 +0100

    commit 9

--- csv_parser/__init__.py
+++ csv_parser/__init__.py
@@ -1,3 +1,44 @@
 """
 Package CSV Parser
 """
+import os # imporetr la biblioteque os pour gerer la pause a la fin de script
+import requests # importer la bibliotheque request pour charger page et la stocker dans une variable
+from requests.exceptions import HTTPError
+import urllib.request
+from os import getcwd, chdir, mkdir
+from os import chdir
+import os
+rep_cour = os.getcwd()
+print(rep_cour)
+chdir("csv_uploads")
+rep_cour = os.getcwd()
+print(rep_cour)
+
+# recuprer la page avec request.get qui lance une requete (http/https)
+for url in ['https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv', 'https://people.sc.fsu.edu/~jburkardt/data/csv/airtravel.csv','https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv']:
+    try:
+        # telecharegr le fichier et le stocker dans 
+        chaine = url.split("/")
+        name_file = chaine[len(chaine) - 1] 
+        #name_files = r"C:\Users\soray\Documents\cours python\Projets\csv-parser\csv_parser\csv_uploads"
+       
+        #if not os.path.isfile(name_files){
+    
+        with open(name_file, 'wb') as file:
+            # get request
+            response = requests.get(url) # response est un objet qui permet d'inspecter le resultat de la requete 
+            # write to file
+            file.write(response.content)
+            # si response est successful, no Exception will be raised 
+            response.raise_for_status() # un HTTPErrorsera déclenché pour certains codes d'état si le code d'etat indique reussit il continu 
+           # }
+           
+       
+    except HTTPError as http_err:
+                print(f'HTTP error occurred: {http_err}')   
+    except Exception as err:
+        print(f'Other error occurred: {err}')  
+    else:
+        print('Success!')
+
+os.path.exists('/Users/toto/Desktop')
