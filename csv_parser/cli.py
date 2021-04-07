422213c1a0ccee3987d5945f516db58e5550fa6f
soraya <sorayaeldjama@gmail.com>
2021-04-07 22:47:36 +0100

    add functions

--- csv_parser/cli.py
+++ csv_parser/cli.py
@@ -1,3 +1,9 @@
 """
 Cli entrypoint
 """
+import file_handler
+
+
+lien = 'https://people.sc.fsu.edu/~jburkardt/data/csv/'
+url = [lien + 'addresses.csv', lien + 'airtravel.csv',
+        lien + 'biostats.csv', lien + "cities.csv", ]
--- csv_parser/file_handler.py
+++ csv_parser/file_handler.py
@@ -1,43 +1,44 @@
 """
 Package CSV Parser
 """
-import os  # imporetr la biblioteque os pour gerer la pause a la fin de script
-import requests  # importer la bibliotheque request pour charger page et la stocker dans une variable
+import os  
+import requests 
 from requests.exceptions import HTTPError
-from os import getcwd, chdir  # importer ce module pour vhanger de repertoire
+from os import getcwd, chdir  
 
+"""
 rep_cour = getcwd()  # connaitre notre repertoire actuel
 print(rep_cour)
 chdir("csv_uploads")
 rep_cour = os.getcwd()
 print(rep_cour)
+"""
 
-# recuprer la page avec request.get qui lance une requete (http/https)
-lien = 'https://people.sc.fsu.edu/~jburkardt/data/csv/'
-for url in [lien + 'addresses.csv', lien + 'airtravel.csv',
-            lien + 'biostats.csv', lien + "cities.csv", ]:
+def download_page():
     try:
-        # telecharegr le fichier et le stocker dans
-        chaine = url.split("/")
-        name_file = chaine[len(chaine) - 1]        
-        if os.path.isfile(name_file):
-            print('le fichier existe ')
-            
-        else:
-            print('le fichier n existe pas')
-            with open(name_file, 'wb') as file:
-                # get request
-                response = requests.get(url)  # response est un objet qui permet d'inspecter le resultat de la requete 
-                # write to file
-                file.write(response.content)
-                # si response est successful, no Exception will be raised 
-                response.raise_for_status() # un HTTPErrorsera déclenché pour certains codes d'état si le code d'etat indique reussit il continu 
-           
-       
+        #  get request, response est un objet
+        response = requests.get(url)
     except HTTPError as http_err:
-                print(f'HTTP error occurred: {http_err}')   
+        print(f'HTTP error occurred: {http_err}')   
     except Exception as err:
         print(f'Other error occurred: {err}')  
     else:
-        print('Success!')
+        print('Success!, le lien est telecharger')
 
+
+def name_file(url):
+    chaine = url.split("/")
+    name_file = chaine[len(chaine) - 1]        
+
+
+def write_file(response):
+    with open(name_file, 'wb') as file:
+        # write to file
+        file.write(response.content)
+        # si response est successful, no Exception will be raised 
+        response.raise_for_status()  # un HTTPErrorsera déclenché pour certains codes d'état
+
+
+def file_exist():
+    if os.path.isfile(name_file):
+        print('le fichier existe ')            
