f9b02993a3b74d012ef4b2d4fee69a018bcad895
soraya <sorayaeldjama@gmail.com>
2021-04-08 17:08:58 +0100

    file_exists and write_file

--- csv_parser/cli.py
+++ csv_parser/cli.py
@@ -1,12 +1,22 @@
 """
 Cli entrypoint
 """
-from file_handler import download_page, name_file, write_file, file_exist
+import os
+from file_handler import download_page, name_files, write_file, file_exist
 
 
 lien = 'https://people.sc.fsu.edu/~jburkardt/data/csv/'
 urls = [lien + 'addresses.csv', lien + 'airtravel.csv',
         lien + 'biostats.csv', lien + "cities.csv", ]
 for url in urls:
-    download_page(url)  
+    response = download_page(url)
+    name_file = name_files(url)
+    print(name_file)
+    if not file_exist(name_file):
+        write_file(name_file, response)
+    else:
+        print("le fichier existe deja")   
+   
+
+
     
--- csv_parser/file_handler.py
+++ csv_parser/file_handler.py
@@ -19,27 +19,35 @@ def download_page(url):
     # Telacharger la page csv
     try:
         response = requests.get(url)
+        # si response est successful, no Exception will be raised 
+        response.raise_for_status()  # un HTTPErrorsera déclenché pour certains codes d'état
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')   
     except Exception as err:
         print(f'Other error occurred: {err}')  
     else:
         print('Success!, le lien est telecharger')
+    return response
 
 
-def name_file(url):
-    chaine = url.split("/")
-    name_file = chaine[len(chaine) - 1]        
+def name_files(url):
+    chaine = url.split("/")     
+    return chaine[len(chaine) - 1]
 
 
-def write_file(response):
-    with open(name_file, 'wb') as file:
-        # write to file
+def write_file(name_file, response):
+    path = os.getcwd()
+    with open(path + '/csv_uploads/' + name_file, 'wb') as file:
         file.write(response.content)
-        # si response est successful, no Exception will be raised 
-        response.raise_for_status()  # un HTTPErrorsera déclenché pour certains codes d'état
+        
 
-
-def file_exist():
-    if os.path.isfile(name_file):
-        print('le fichier existe ')            
+def file_exist(name_file):
+    path = os.getcwd()
+    try:
+        if os.path.isfile(path + '/csv_uploads/' + name_file):              
+            print('le fichier existe ') 
+        else:
+            print("il n existe pas")
+                      
+    except:
+        print("le repertoire n existe pas")
