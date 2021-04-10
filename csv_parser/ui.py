cca7208e72477da13c5891781039955eaa86a6ee
soraya <sorayaeldjama@gmail.com>
2021-04-10 20:46:23 +0100

    ui

--- csv_parser/ui.py
+++ csv_parser/ui.py
@@ -8,11 +8,15 @@ from cli import download
 
 #  ui.py -> user interface
 #  gui.py -> graphical user interface
+lien = "https://people.sc.fsu.edu/~jburkardt/data/csv/"
 print("voici notre menu selectionnez l'annalyse que vous souhaiter")
 print("fichier 1 a parser")
 fichier1 = input("tapez  addresses pour parser le fichier 1")
 download(fichier1)
-input("tapez cities pour parser le fichier cities")
+afficher1 = input("tapper 1 pour afficher le fichier addresses")
+
+fichier2 = input("tapez cities pour parser le fichier cities")
+download(fichier2)
 
 print("Fichier 2 a parser")
 print("Fichier 3 a parser")
