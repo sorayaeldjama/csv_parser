d1f6587b600ae2b58cda3e212e3e7012a209bbb1
soraya <sorayaeldjama@gmail.com>
2021-04-10 18:52:33 +0100

    add pycache to gitignore

--- csv_parser/cli.py
+++ csv_parser/cli.py
@@ -9,13 +9,14 @@ lien = 'https://people.sc.fsu.edu/~jburkardt/data/csv/'
 urls = [lien + 'addresses.csv', lien + 'airtravel.csv',
         lien + 'biostats.csv', lien + "cities.csv", ]
 for url in urls:
-    response = download_page(url)
-    name_file = name_files(url)
-    print(name_file)
-    if not file_exist(name_file):
-        write_file(name_file, response)
-    else:
-        print("le fichier existe deja")   
+    def download(fichier):
+        response = download_page(lien + fichier)
+        name_file = name_files(lien + fichier)
+        print(name_file)
+        if not file_exist(name_file):
+            write_file(name_file, response)
+        else:
+            print("le fichier existe deja")   
    
 
 
--- csv_parser/ui.py
+++ csv_parser/ui.py
@@ -1,10 +1,18 @@
 """
 User  Interface module.
 """
-#ui.py -> user interface
-#gui.py -> graphical user interface
+
+
+from cli import download
+
+
+#  ui.py -> user interface
+#  gui.py -> graphical user interface
 print("voici notre menu selectionnez l'annalyse que vous souhaiter")
 print("fichier 1 a parser")
+fichier1 = input("tapez  addresses pour parser le fichier 1")
+download(fichier1)
 input("tapez cities pour parser le fichier cities")
+
 print("Fichier 2 a parser")
 print("Fichier 3 a parser")
