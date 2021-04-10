1806d1cd81fc95324bd63cbde0dcdc25b1f37fbc
soraya <sorayaeldjama@gmail.com>
2021-04-10 23:28:08 +0100

    affichaage_csv

--- csv_parser/cli.py
+++ csv_parser/cli.py
@@ -1,23 +1,31 @@
 """
 Cli entrypoint
 """
-import os
+import os, csv
 from file_handler import download_page, name_files, write_file, file_exist
 
 
 lien = 'https://people.sc.fsu.edu/~jburkardt/data/csv/'
-urls = [lien + 'addresses.csv', lien + 'airtravel.csv',
-        lien + 'biostats.csv', lien + "cities.csv", ]
-for url in urls:
-    def download(fichier):
-        response = download_page(lien + fichier)
-        name_file = name_files(lien + fichier)
-        print(name_file)
-        if not file_exist(name_file):
-            write_file(name_file, response)
-        else:
-            print("le fichier existe deja")   
-   
+# urls = [lien + 'addresses.csv', lien + 'airtravel.csv',
+#  lien + 'biostats.csv', lien + "cities.csv", ]
+# for url in urls:
 
 
-    
+def download(fichier):
+    response = download_page(lien + fichier + ".csv")
+    name_file = name_files(lien + fichier + ".csv")
+    print(name_file)
+    if not file_exist(name_file):
+        write_file(name_file, response)
+    else:
+        print("le fichier existe deja")
+
+
+def afficher(fichier):
+    path = os.getcwd()
+    f = open(path + "\\csv_uploads\\" + fichier + ".csv")
+    myReader = csv.reader(f)
+    for row in myReader:
+        print(row)
+  
+
--- csv_parser/ui.py
+++ csv_parser/ui.py
@@ -3,7 +3,7 @@ User  Interface module.
 """
 
 
-from cli import download
+from cli import download, afficher
 
 
 #  ui.py -> user interface
@@ -14,9 +14,11 @@ print("fichier 1 a parser")
 fichier1 = input("tapez  addresses pour parser le fichier 1")
 download(fichier1)
 afficher1 = input("tapper 1 pour afficher le fichier addresses")
+afficher(fichier1)
 
 fichier2 = input("tapez cities pour parser le fichier cities")
 download(fichier2)
+
 
 print("Fichier 2 a parser")
 print("Fichier 3 a parser")
