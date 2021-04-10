6627a7bd36c23afb1f9a5ec07222d424ce738de9
soraya <sorayaeldjama@gmail.com>
2021-04-10 23:45:28 +0100

    ui changes

--- csv_parser/ui.py
+++ csv_parser/ui.py
@@ -1,24 +1,24 @@
 """
 User  Interface module.
 """
-
+#  ui.py -> user interface
+#  gui.py -> graphical user interface
 
 from cli import download, afficher
 
 
-#  ui.py -> user interface
-#  gui.py -> graphical user interface
 lien = "https://people.sc.fsu.edu/~jburkardt/data/csv/"
 print("voici notre menu selectionnez l'annalyse que vous souhaiter")
+
 print("fichier 1 a parser")
 fichier1 = input("tapez  addresses pour parser le fichier 1")
 download(fichier1)
+
 afficher1 = input("tapper 1 pour afficher le fichier addresses")
 afficher(fichier1)
 
 fichier2 = input("tapez cities pour parser le fichier cities")
 download(fichier2)
-
 
 print("Fichier 2 a parser")
 print("Fichier 3 a parser")
